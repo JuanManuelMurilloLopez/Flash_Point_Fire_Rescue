217f29ab965e0d13ea3d7d7d0fe2f7b2ccf444ba
SALV <a01707122@tec.mx>
2025-08-27 15:29:59 -0600

    [FIX] schema POI, Damage, Dices

--- MultiAgent System/Backend/app.py
+++ MultiAgent System/Backend/app.py
@@ -26,6 +26,12 @@ dict = {
             {"state": "fire", "position": {"x": 14, "z": 10}},
             {"state": "explosion", "position": {"x": 14, "z": 10}},
         ],
+        "dice": {"red": 1, "black": 5},
+        "poi": [
+            {"state": "fake", "position": {"x": 10, "z": 10}},
+            {"state": "fake", "position": {"x": 10, "z": 10}},
+        ],
+        "damage": 5,
     },
     2: {
         "players": [
