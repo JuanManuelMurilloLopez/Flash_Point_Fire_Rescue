efa592e0f89e16d13e5e7b80f5aa7664e2ae93a3
SALV <a01707122@tec.mx>
2025-09-02 17:40:58 -0600

    [FIX] split FireAgent into separate files

--- MultiAgent System/Models/Firefighter/__init__.py
+++ MultiAgent System/Models/Firefighter/__init__.py
@@ -12,6 +12,8 @@ class Firefighter(Agent):
         self.carryingVictim = False
         self.knockedDown = False
 
+        from strategy import searchForPOIs
+
     # TODO: Terminar lógica del step
     def step(self):
         while self.actionPoints > 0:
@@ -124,59 +126,3 @@ class Firefighter(Agent):
                 door.destroy()
 
             self.actionPoints -= 2
-
-    def searchForPOIs(self):
-        self.chooseEntry()
-        poiPosition = self.selectPOI()
-        self.makeRoute(poiPosition)
-
-    def chooseEntry(self):
-        entrance = np.random.randint(len(self.model.entrances))
-        self.model.grid.move_agent(self.model.entrances[entrance])
-
-    def selectPOI(self):
-        cells = self.model.cells
-
-        POIfound = False
-
-        queue = deque()
-        queue.append(self.pos)
-        visited = set(self.pos)
-
-        while not POIfound:
-            path = queue.popleft()
-            x, y = path[-1]
-
-            if cells[x][y].poi and (x, y) not in self.model.POIsFound:
-                POIfound = True
-                self.model.POIsFound.add((x, y))
-                return (x, y)
-
-            neighbors = self.model.getNeighbors(x, y)
-
-            for nX, nY in neighbors:
-                if (nX, nY) not in visited:
-                    visited.add((nX, nY))
-                    queue.append(path + [(nX, nY)])
-
-    def makeRoute(self, position):
-        pq = PriorityQueue()
-
-        pq.push(0, self.pos)
-        x, y = self.pos
-        dist = np.zeros(self.model.width * self.model.height)
-        dist[self.to_int(x, y)] = 0
-        while not pq.empty():
-            currentDistance, u = pq.top()
-            pq.pop()
-
-            currentCell = self.model.cells[x][y]
-
-            if u == position:
-                break
-
-            for nX, nY in self.model.getNeighbors(x, y):
-                newDistance = dist[self.to_int(x, y)] + 1
-
-    def to_int(self, x, y):
-        return x + self.model.height * y
