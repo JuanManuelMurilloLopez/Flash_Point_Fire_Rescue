84c7731b794faaccc2a363490b53a59dfc826caa
SALV <a01707122@tec.mx>
2025-09-10 10:22:08 -0600

    [FIX] API helper, close connection after fetching information

--- Assets/Scripts/API/Backend/APIHelper.cs
+++ Assets/Scripts/API/Backend/APIHelper.cs
@@ -7,17 +7,12 @@ public static class APIHelper
 {
     public static Response GetStep(int number)
     {
-        Debug.Log($"Fetching Webrequest {number}");
         HttpWebRequest request = (HttpWebRequest) WebRequest.Create($"http://localhost:5000/step/{number}");
-        Debug.Log($"Created Request");
-        HttpWebResponse response = (HttpWebResponse) request.GetResponse();
-        Debug.Log($"Response gotten");
-
-        StreamReader reader = new StreamReader(response.GetResponseStream());
-
-        string json = reader.ReadToEnd();
-        Debug.Log($"Response: {json}");
-        
-        return JsonUtility.FromJson<Response>(json);
+        using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+        using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+        {
+            string json = reader.ReadToEnd();
+            return JsonUtility.FromJson<Response>(json);
+        }
     }
 }
